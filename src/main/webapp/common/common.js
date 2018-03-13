ed86414ad7bd15f0c8dc13fb94f34c2e39839b65
kuanghaohua <kuanghaohua@do1.com.cn>
2018-03-13 22:12:29 +0800

    登陆后端开发

---
+++ src/main/webapp/common/common.js
@@ -0,0 +1,9 @@
+/**
+ * Created by admin on 2018/3/13.
+ */
+
+var localhost_project = "http://localhost/sharesWechat";
+
+var server_ip = "";
+
+var server_project = "http://" + server_ip + "/sharesWechat";
